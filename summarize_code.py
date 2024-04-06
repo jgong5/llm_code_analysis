081ff184f0d3fbfb6817d9390257568def9c645b
Jiong Gong <jiong.gong@intel.com>
2024-04-06 14:34:46 +0800

    first commit

---
+++ summarize_code.py
@@ -0,0 +1,71 @@
+# Reference: https://python.langchain.com/docs/use_cases/code_understanding/
+
+from git import Repo
+from langchain_community.document_loaders.generic import GenericLoader
+from langchain_community.document_loaders.parsers import LanguageParser
+from langchain_text_splitters import Language
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+import dotenv
+dotenv.load_dotenv()
+
+repo_path = "/Users/jacoblee/Desktop/test_repo"
+repo = Repo.clone_from("https://github.com/langchain-ai/langchain", to_path=repo_path)
+
+loader = GenericLoader.from_filesystem(
+    repo_path + "/libs/core/langchain_core",
+    glob="**/*",
+    suffixes=[".py"],
+    exclude=["**/non-utf8-encoding.py"],
+    parser=LanguageParser(language=Language.PYTHON, parser_threshold=500),
+)
+documents = loader.load()
+
+python_splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON, chunk_size=2000, chunk_overlap=200
+)
+texts = python_splitter.split_documents(documents)
+
+db = Chroma.from_documents(texts, OpenAIEmbeddings(disallowed_special=()))
+retriever = db.as_retriever(
+    search_type="mmr",  # Also test "similarity"
+    search_kwargs={"k": 8},
+)
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("placeholder", "{chat_history}"),
+        ("user", "{input}"),
+        (
+            "user",
+            "Given the above conversation, generate a search query to look up to get information relevant to the conversation",
+        ),
+    ]
+)
+
+retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Answer the user's questions based on the below context:\n\n{context}",
+        ),
+        ("placeholder", "{chat_history}"),
+        ("user", "{input}"),
+    ]
+)
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+qa = create_retrieval_chain(retriever_chain, document_chain)
+question = "What is a RunnableBinding?"
+result = qa.invoke({"input": question})
+print(result["answer"])
